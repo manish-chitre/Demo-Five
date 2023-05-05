cf6a41f2a4baf9892be44101415520a604a4b365
Manish Chitre <manish_chitre@hotmail.com>
2023-05-05 22:54:29 +0530

    added few lines in first.js and secound.js

--- first.js
+++ first.js
@@ -1,3 +1,6 @@
 function first(){
     console.log('This is first sign of defence');
 }
+console.log('This is first file of yours please check');
+console.log('This is secound file of yours please check');
+console.log('This is third file of yours please check');
--- secound.js
+++ secound.js
@@ -2,3 +2,4 @@ function secound() {
   console.log("This is secound file");
 }
 console.log('your own secound file');
+console.log('I am adding one more example here please check if possible');
